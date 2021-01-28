7b4f732cfd2236020f7342ddebf68a2a8a1f29c2
Meryem Yanar <meryem.yanaryanar@gmail.com>
2021-01-29 01:25:00 +0300

    Import theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import Header from './components/organisms/Header'
+import theme from './theme'
 
 function App() {
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={theme}>
       <Header />
     </ThemeProvider>
   )
