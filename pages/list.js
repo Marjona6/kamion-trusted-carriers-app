c4c7221b9eabcbf8b4ce7c1af6fd183fb0732dd3
Meryem Yanar <meryem.yanaryanar@gmail.com>
2021-01-31 21:21:35 +0300

    Fix add carrier modal import

--- pages/list.js
+++ pages/list.js
@@ -4,7 +4,7 @@ import Header from '../src/components/templates/Header'
 import H2 from '../src/components/atoms/texts/H2'
 
 import CarrierList from '../src/components/templates/CarrierList'
-import AddCarrierModal from '../src/components/organisms/AddCarrierModal'
+import AddCarrierModal from '../src/components/templates/AddCarrierModal'
 
 const List = () => {
   const [showModal, setShowModal] = useState(false)
